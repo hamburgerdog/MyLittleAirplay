4446b16bc40660bcc0661acfa45f421a3ce48396
hamburgerdog <838980000@qq.com>
2021-12-22 22:53:00 +0800

    style(eslint): vue-cli eslint

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const resolve = dir => path.join(__dirname, dir);
+
+const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.alias
-      .set('@', resolve('src'))
-  }
-}
+      .set('@', resolve('src'));
+  },
+};
